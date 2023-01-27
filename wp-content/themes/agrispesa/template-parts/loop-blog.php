9d1c6178d1fd10fab104adb161578825b6a9a756
Nathalie J. Schwitter <nathalieschwitter@gmail.com>
2023-01-27 10:35:08 +0100

    fix blog

--- wp-content/themes/agrispesa/template-parts/loop-blog.php
+++ wp-content/themes/agrispesa/template-parts/loop-blog.php
@@ -1,6 +1,6 @@
 <?php
 $thumb_id = get_post_thumbnail_id();
-$thumb_url_array = wp_get_attachment_image_src($thumb_id, 'medium', true);
+$thumb_url_array = wp_get_attachment_image_src($thumb_id, 'large', true);
 $thumb_url = $thumb_url_array[0];
 ?>
 <article class="magazine-post">
