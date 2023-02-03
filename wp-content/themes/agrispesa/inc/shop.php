8a5570e15f8a761e847699b66bb0fcdf2a57172d
Nathalie J. Schwitter <nathalieschwitter@gmail.com>
2023-02-03 16:46:15 +0100

    test checkout

--- wp-content/themes/agrispesa/inc/shop.php
+++ wp-content/themes/agrispesa/inc/shop.php
@@ -208,3 +208,8 @@ function coupon_free_shipping_customization( $rates, $package ) {
     }
     return $rates;
 }
+
+//Sposta bottoni di pagamento prima del bottone di default
+add_filter('woocommerce_paypal_payments_checkout_button_renderer_hook', function() {
+    return 'woocommerce_review_order_before_submit';
+});
