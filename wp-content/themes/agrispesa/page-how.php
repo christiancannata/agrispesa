541e79508091f9cab9e4f6eeb89a5a9e5a386db7
Nathalie J. Schwitter <nathalieschwitter@gmail.com>
2023-02-09 13:23:52 +0100

    acf

--- wp-content/themes/agrispesa/page-how.php
+++ wp-content/themes/agrispesa/page-how.php
@@ -6,17 +6,9 @@ get_header(); ?>
 
 <div class="wrapper" id="index-wrapper">
 
-
-	<?php get_template_part( 'global-elements/hero', 'page' ); ?>
-
 	<section class="how-works-page">
 		<?php get_template_part( 'global-elements/home', 'sections' ); ?>
 	</section>
-
-
-
-
-
 
 
 </div>
--- wp-content/themes/agrispesa/page-reviews.php
+++ wp-content/themes/agrispesa/page-reviews.php
@@ -7,7 +7,9 @@ get_header(); ?>
 <div class="wrapper" id="index-wrapper">
 
 
-	<?php get_template_part( 'global-elements/hero', 'page' ); ?>
+	<section class="how-works-page">
+		<?php get_template_part( 'global-elements/home', 'sections' ); ?>
+	</section>
 
 	<!-- TrustBox widget - Micro Review Count -->
 	<div class="trustpilot-widget" data-locale="it-IT" data-template-id="5419b6a8b0d04a076446a9ad" data-businessunit-id="61ee37eca9b063488e95b01d" data-style-height="24px" data-style-width="100%" data-theme="light" data-min-review-count="10" data-style-alignment="center">
