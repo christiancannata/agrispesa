6a6aba7f2d3eff31cfdaea87424e5b2e4df0d1db
Nathalie J. Schwitter <nathalieschwitter@gmail.com>
2023-02-09 13:14:42 +0100

    acf

--- wp-content/themes/agrispesa/page-shipping.php
+++ wp-content/themes/agrispesa/page-shipping.php
@@ -6,41 +6,9 @@ get_header(); ?>
 
 <div class="wrapper" id="index-wrapper">
 
-
-	<section class="manifesto--hero">
-		<div class="manifesto--container">
-				<h1 class="manifesto--hero--title">Consegna a domicilio</h1>
-				<h2 class="manifesto--hero--subtitle">
-					Prepariamo la spesa e la consegniamo a casa tua, una volta a settimana.
-					La portiamo in ogni città del Piemonte, della Lombardia, della Liguria e in alcuni comuni della Valle d'Aosta.
-				</h2>
-		</div>
+	<section class="shipping-home-page">
+		<?php get_template_part( 'global-elements/home', 'sections' ); ?>
 	</section>
-
-
-	<section class="manifesto--video">
-		<div class="videoWrapper">
-			<video width="320" height="240" autoplay loop muted>
-				<source src="<?php echo get_template_directory_uri(); ?>/assets/video/farmer-3.mp4" type="video/mp4">
-			</video>
-		</div>
-	</section>
-
-	<section class="manifesto--hero">
-		<div class="manifesto--container">
-				<h3 class="manifesto--hero--subtitle">
-					All’interno della scatola collochiamo i prodotti con attenzione, per occupare al meglio lo spazio, perché siano protetti durante le fasi di trasporto e affinchè aprire la scatola sia una gioia per gli occhi e per il cuore.
-				</h3>
-		</div>
-	</section>
-
-
-
-
-
-
-
-
 
 </div>
 
