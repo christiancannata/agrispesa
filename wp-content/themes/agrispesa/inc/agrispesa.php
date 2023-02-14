5b0476a358547000392307615c6d64b687876068
Nathalie J. Schwitter <nathalieschwitter@gmail.com>
2023-02-14 21:49:28 +0100

    fix

--- wp-content/themes/agrispesa/inc/agrispesa.php
+++ wp-content/themes/agrispesa/inc/agrispesa.php
@@ -95,6 +95,8 @@ function misha_remove_my_account_links( $menu_links ){
 	unset( $menu_links['downloads'] ); // Disable Downloads
 
   $menu_links['gift-cards'] = __('Carte Regalo', 'textdomain');
+  $menu_links['edit-address'] = __('Indirizzo', 'woocommerce');
+  $menu_links['edit-account'] = __('Account', 'woocommerce');
   $menu_links['customer-logout'] = __('Esci', 'textdomain');
 
 	return $menu_links;
