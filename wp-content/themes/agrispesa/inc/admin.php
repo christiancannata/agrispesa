6d1c1806e61cd3617511915efc59515d6ec4de57
christian cannata <christian1488@hotmail.it>
2023-02-17 15:40:29 +0700

    minor fix

--- wp-content/themes/agrispesa/inc/admin.php
+++ wp-content/themes/agrispesa/inc/admin.php
@@ -2385,7 +2385,7 @@ function my_saved_post($post_id, $json, $is_update)
 	$price = number_format(floatval($price), 2);
 
 	update_post_meta($post_id, '_price', $price);
-	update_post_meta($post_id, '_regular_price', $price, 2));
+	update_post_meta($post_id, '_regular_price', $price, 2);
 
 	update_post_meta($post_id, '_ricarico_percentuale', $record['_ricarico_percentuale']);
 	update_post_meta($post_id, '_prezzo_acquisto', number_format($json['costounitario'], 2));
