a8cff45273c996f2b6eae7ef7c2f68bda6a2691b
Nathalie J. Schwitter <nathalieschwitter@gmail.com>
2023-02-21 13:59:09 +0100

    fix negozio

--- wp-content/themes/agrispesa/inc/shop.php
+++ wp-content/themes/agrispesa/inc/shop.php
@@ -340,3 +340,13 @@ function category_has_children( $term_id = 0, $taxonomy = 'product_cat' ) {
     ) );
     return ( $children );
 }
+
+/*** Change number of products that are displayed per page (shop page)*/
+add_filter( 'loop_shop_per_page', 'new_loop_shop_per_page', 20 );
+
+function new_loop_shop_per_page( $cols ) {
+  // $cols contains the current number of products per page based on the value stored on Options –> Reading
+  // Return the number of products you wanna show per page.
+  $cols = 20;
+  return $cols;
+}
