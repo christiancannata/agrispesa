b5587d43d7a296712f33d9addf906575a03a52f9
Nathalie J. Schwitter <nathalieschwitter@gmail.com>
2023-03-03 10:19:21 +0100

    test stampante

---
+++ wp-content/themes/agrispesa/page-test.php
@@ -0,0 +1,27 @@
+<?php
+/* Template Name: TEST */
+
+get_header();
+
+
+
+
+?>
+
+
+<div class="wrapper" id="index-wrapper" style="min-height:500px;">
+
+	<form>
+	<input type="button" name="Submit" value="Stampa" onclick="window.print()">
+	</form>
+
+</div>
+
+
+
+
+
+</div>
+
+<?php
+get_footer();
