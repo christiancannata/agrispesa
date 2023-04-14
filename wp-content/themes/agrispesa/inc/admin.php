dc8580d7bc3f18f9686a69e252af3027cac40ab8
christian cannata <christian1488@hotmail.it>
2023-04-14 16:00:33 -0500

    minor fix

--- wp-content/themes/agrispesa/inc/admin.php
+++ wp-content/themes/agrispesa/inc/admin.php
@@ -355,7 +355,7 @@ add_action("rest_api_init", function () {
 
             $hasSubscription = wcs_user_has_subscription($wordpressUser->ID);
             if (!$hasSubscription) {
-				 $wpdb->query("DELETE p from wp_posts p,wp_postmeta m  WHERE m.post_id = p.ID and p.post_type = 'shop_subscription' and m.meta_key = '_customer_user' and m.meta_value = " . $wordpressUser->ID);
+				// $wpdb->query("DELETE p from wp_posts p,wp_postmeta m  WHERE m.post_id = p.ID and p.post_type = 'shop_subscription' and m.meta_key = '_customer_user' and m.meta_value = " . $wordpressUser->ID);
 
                 if (empty($user["automatismiSettimanali_utenze_tipoSpesa::codiceTipoSpesa"]) || !isset($user["tipoSpesaCalcolato"])) {
                     continue;
