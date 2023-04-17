bf8c1c69772c129da4fee4436d7464be7151b91e
Nathalie J. Schwitter <nathalieschwitter@gmail.com>
2023-04-17 10:11:59 +0200

    thanks

--- wp-content/themes/agrispesa/page-test.php
+++ wp-content/themes/agrispesa/page-test.php
@@ -1,5 +1,5 @@
 <?php
-/* Template Name: TEST */
+/* Template Name: Thank you - Newsletter */
 
 get_header();
 
@@ -9,12 +9,10 @@ get_header();
 ?>
 
 
-<div class="wrapper" id="index-wrapper" style="min-height:500px;">
-
-	<form>
-	<input type="button" name="Submit" value="Stampa" onclick="window.print()">
-	</form>
-
+<div class="thanks">
+  <h2 class="thanks--title">Sei dei nostri!</h2>
+  <p class="thanks--subtitle">Grazie, adesso fai parte della nostra mailing list.<br/>Ti terremo aggiornat* sulle novità dell'orto.</p>
+  <a href="<?php echo esc_url( home_url( '/' ) ); ?>box/facciamo-noi/" title="Fai la spesa" class="btn btn-primary">Fai la spesa!</a>
 </div>
 
 
