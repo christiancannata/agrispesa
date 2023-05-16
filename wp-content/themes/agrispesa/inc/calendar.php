4ff944eef6cdc254e6cf493fb37ab2c7de78269f
christian cannata <christian1488@hotmail.it>
2023-05-16 11:23:31 -0500

    minor fix

--- wp-content/themes/agrispesa/inc/calendar.php
+++ wp-content/themes/agrispesa/inc/calendar.php
@@ -1,213 +1,220 @@
 <?php
 
-class Calendar {
+class Calendar
+{
 
-    /**
-     * Constructor
-     */
-    public function __construct(){
-        $this->naviHref = htmlentities($_SERVER['PHP_SELF']);
-    }
+	/**
+	 * Constructor
+	 */
+	public function __construct()
+	{
+		$this->naviHref = htmlentities($_SERVER['PHP_SELF']);
+	}
 
-    /********************* PROPERTY ********************/
-    private $dayLabels = array("Mon","Tue","Wed","Thu","Fri","Sat","Sun");
+	/********************* PROPERTY ********************/
+	private $dayLabels = array("Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun");
 
-    private $currentYear=0;
+	private $currentYear = 0;
 
-    private $currentMonth=0;
+	private $currentMonth = 0;
 
-    private $currentDay=0;
+	private $currentDay = 0;
 
-    private $currentDate=null;
+	private $currentDate = null;
 
-    private $daysInMonth=0;
+	private $daysInMonth = 0;
 
-    private $naviHref= null;
+	private $naviHref = null;
 
-    /********************* PUBLIC **********************/
+	/********************* PUBLIC **********************/
 
-    /**
-    * print out the calendar
-    */
-    public function show() {
-        $year  = null;
+	/**
+	 * print out the calendar
+	 */
+	public function show()
+	{
+		$year = null;
 
-        $month = null;
+		$month = null;
 
-        if(null==$year&&isset($_GET['year'])){
+		if (null == $year && isset($_GET['year'])) {
 
-            $year = $_GET['year'];
+			$year = $_GET['year'];
 
-        }else if(null==$year){
+		} else if (null == $year) {
 
-            $year = date("Y",time());
+			$year = date("Y", time());
 
-        }
+		}
 
-        if(null==$month&&isset($_GET['month'])){
+		if (null == $month && isset($_GET['month'])) {
 
-            $month = $_GET['month'];
+			$month = $_GET['month'];
 
-        }else if(null==$month){
+		} else if (null == $month) {
 
-            $month = date("m",time());
+			$month = date("m", time());
 
-        }
+		}
 
-        $this->currentYear=$year;
+		$this->currentYear = $year;
 
-        $this->currentMonth=$month;
+		$this->currentMonth = $month;
 
-        $this->daysInMonth=$this->_daysInMonth($month,$year);
+		$this->daysInMonth = $this->_daysInMonth($month, $year);
 
-        $content='<div id="calendar">'.
-                        '<div class="box">'.
-                        $this->_createNavi().
-                        '</div>'.
-                        '<div class="box-content">'.
-                                '<ul class="label">'.$this->_createLabels().'</ul>';
-                                $content.='<div class="clear"></div>';
-                                $content.='<ul class="dates">';
+		$content = '<div id="calendar">' .
+			'<div class="box">' .
+			$this->_createNavi() .
+			'</div>' .
+			'<div class="box-content">' .
+			'<ul class="label">' . $this->_createLabels() . '</ul>';
+		$content .= '<div class="clear"></div>';
+		$content .= '<ul class="dates">';
 
-                                $weeksInMonth = $this->_weeksInMonth($month,$year);
-                                // Create weeks in a month
-                                for( $i=0; $i<$weeksInMonth; $i++ ){
+		$weeksInMonth = $this->_weeksInMonth($month, $year);
+		// Create weeks in a month
+		for ($i = 0; $i < $weeksInMonth; $i++) {
 
-                                    //Create days in a week
-                                    for($j=1;$j<=7;$j++){
-                                        $content.=$this->_showDay($i*7+$j);
-                                    }
-                                }
+			//Create days in a week
+			for ($j = 1; $j <= 7; $j++) {
+				$content .= $this->_showDay($i * 7 + $j);
+			}
+		}
 
-                                $content.='</ul>';
+		$content .= '</ul>';
 
-                                $content.='<div class="clear"></div>';
+		$content .= '<div class="clear"></div>';
 
-                        $content.='</div>';
+		$content .= '</div>';
 
-        $content.='</div>';
-        return $content;
-    }
+		$content .= '</div>';
+		return $content;
+	}
 
-    /********************* PRIVATE **********************/
-    /**
-    * create the li element for ul
-    */
-    private function _showDay($cellNumber){
+	/********************* PRIVATE **********************/
+	/**
+	 * create the li element for ul
+	 */
+	private function _showDay($cellNumber)
+	{
 
-        if($this->currentDay==0){
+		if ($this->currentDay == 0) {
 
-            $firstDayOfTheWeek = date('N',strtotime($this->currentYear.'-'.$this->currentMonth.'-01'));
+			$firstDayOfTheWeek = date('N', strtotime($this->currentYear . '-' . $this->currentMonth . '-01'));
 
-            if(intval($cellNumber) == intval($firstDayOfTheWeek)){
+			if (intval($cellNumber) == intval($firstDayOfTheWeek)) {
 
-                $this->currentDay=1;
+				$this->currentDay = 1;
 
-            }
-        }
+			}
+		}
 
-        if( ($this->currentDay!=0)&&($this->currentDay<=$this->daysInMonth) ){
+		if (($this->currentDay != 0) && ($this->currentDay <= $this->daysInMonth)) {
 
-            $this->currentDate = date('Y-m-d',strtotime($this->currentYear.'-'.$this->currentMonth.'-'.($this->currentDay)));
+			$this->currentDate = date('Y-m-d', strtotime($this->currentYear . '-' . $this->currentMonth . '-' . ($this->currentDay)));
 
-            $cellContent = $this->currentDay;
+			$cellContent = $this->currentDay;
 
-            $this->currentDay++;
+			$this->currentDay++;
 
-        }else{
+		} else {
 
-            $this->currentDate =null;
+			$this->currentDate = null;
 
-            $cellContent=null;
-        }
+			$cellContent = null;
+		}
 
 
-        return '<li id="li-'.$this->currentDate.'" class="'.($cellNumber%7==1?' start ':($cellNumber%7==0?' end ':' ')).
-                ($cellContent==null?'mask':'').'">'.$cellContent.'</li>';
-    }
+		return '<li id="li-' . $this->currentDate . '" class="' . ($cellNumber % 7 == 1 ? ' start ' : ($cellNumber % 7 == 0 ? ' end ' : ' ')) .
+			($cellContent == null ? 'mask' : '') . '">' . $cellContent . '</li>';
+	}
 
-    /**
-    * create navigation
-    */
-    private function _createNavi(){
+	/**
+	 * create navigation
+	 */
+	private function _createNavi()
+	{
 
-        $nextMonth = $this->currentMonth==12?1:intval($this->currentMonth)+1;
+		$nextMonth = $this->currentMonth == 12 ? 1 : intval($this->currentMonth) + 1;
 
-        $nextYear = $this->currentMonth==12?intval($this->currentYear)+1:$this->currentYear;
+		$nextYear = $this->currentMonth == 12 ? intval($this->currentYear) + 1 : $this->currentYear;
 
-        $preMonth = $this->currentMonth==1?12:intval($this->currentMonth)-1;
+		$preMonth = $this->currentMonth == 1 ? 12 : intval($this->currentMonth) - 1;
 
-        $preYear = $this->currentMonth==1?intval($this->currentYear)-1:$this->currentYear;
+		$preYear = $this->currentMonth == 1 ? intval($this->currentYear) - 1 : $this->currentYear;
 
-        return
-            '<div class="header">'.
-                '<a class="prev" href="'.$this->naviHref.'?month='.sprintf('%02d',$preMonth).'&year='.$preYear.'">Prev</a>'.
-                    '<span class="title">'.date('Y M',strtotime($this->currentYear.'-'.$this->currentMonth.'-1')).'</span>'.
-                '<a class="next" href="'.$this->naviHref.'?month='.sprintf("%02d", $nextMonth).'&year='.$nextYear.'">Next</a>'.
-            '</div>';
-    }
+		return
+			'<div class="header">' .
+			'<a class="prev" href="' . $this->naviHref . '?month=' . sprintf('%02d', $preMonth) . '&year=' . $preYear . '">Prev</a>' .
+			'<span class="title">' . date('Y M', strtotime($this->currentYear . '-' . $this->currentMonth . '-1')) . '</span>' .
+			'<a class="next" href="' . $this->naviHref . '?month=' . sprintf("%02d", $nextMonth) . '&year=' . $nextYear . '">Next</a>' .
+			'</div>';
+	}
 
-    /**
-    * create calendar week labels
-    */
-    private function _createLabels(){
+	/**
+	 * create calendar week labels
+	 */
+	private function _createLabels()
+	{
 
-        $content='';
+		$content = '';
 
-        foreach($this->dayLabels as $index=>$label){
+		foreach ($this->dayLabels as $index => $label) {
 
-            $content.='<li class="'.($label==6?'end title':'start title').' title">'.$label.'</li>';
+			$content .= '<li class="' . ($label == 6 ? 'end title' : 'start title') . ' title">' . $label . '</li>';
 
-        }
+		}
 
-        return $content;
-    }
+		return $content;
+	}
 
 
+	/**
+	 * calculate number of weeks in a particular month
+	 */
+	private function _weeksInMonth($month = null, $year = null)
+	{
 
-    /**
-    * calculate number of weeks in a particular month
-    */
-    private function _weeksInMonth($month=null,$year=null){
+		if (null == ($year)) {
+			$year = date("Y", time());
+		}
 
-        if( null==($year) ) {
-            $year =  date("Y",time());
-        }
+		if (null == ($month)) {
+			$month = date("m", time());
+		}
 
-        if(null==($month)) {
-            $month = date("m",time());
-        }
+		// find number of days in this month
+		$daysInMonths = $this->_daysInMonth($month, $year);
 
-        // find number of days in this month
-        $daysInMonths = $this->_daysInMonth($month,$year);
+		$numOfweeks = ($daysInMonths % 7 == 0 ? 0 : 1) + intval($daysInMonths / 7);
 
-        $numOfweeks = ($daysInMonths%7==0?0:1) + intval($daysInMonths/7);
+		$monthEndingDay = date('N', strtotime($year . '-' . $month . '-' . $daysInMonths));
 
-        $monthEndingDay= date('N',strtotime($year.'-'.$month.'-'.$daysInMonths));
+		$monthStartDay = date('N', strtotime($year . '-' . $month . '-01'));
 
-        $monthStartDay = date('N',strtotime($year.'-'.$month.'-01'));
+		if ($monthEndingDay < $monthStartDay) {
 
-        if($monthEndingDay<$monthStartDay){
+			$numOfweeks++;
 
-            $numOfweeks++;
+		}
 
-        }
+		return $numOfweeks;
+	}
 
-        return $numOfweeks;
-    }
+	/**
+	 * calculate number of days in a particular month
+	 */
+	private function _daysInMonth($month = null, $year = null)
+	{
 
-    /**
-    * calculate number of days in a particular month
-    */
-    private function _daysInMonth($month=null,$year=null){
+		if (null == ($year))
+			$year = date("Y", time());
 
-        if(null==($year))
-            $year =  date("Y",time());
+		if (null == ($month))
+			$month = date("m", time());
 
-        if(null==($month))
-            $month = date("m",time());
-
-        return date('t',strtotime($year.'-'.$month.'-01'));
-    }
+		return date('t', strtotime($year . '-' . $month . '-01'));
+	}
 
 }
