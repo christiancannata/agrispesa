47b926805a90a89b09bf0c93d3df73888475c529
christian cannata <christian1488@hotmail.it>
2023-06-06 10:06:58 -0400

    minor fix

---
+++ wp-content/cache/min/1/wp-content/plugins/duracelltomi-google-tag-manager/js/analytics-talk-content-tracking.js
@@ -0,0 +1,12 @@
+if("undefined"==typeof console){window.console={log:function(){}}}
+jQuery(function($){var debugMode=gtm4wp_scrollerscript_debugmode;var callBackTime=gtm4wp_scrollerscript_callbacktime;var readerLocation=gtm4wp_scrollerscript_readerlocation;var timer=0;var scroller=!1;var endContent=!1;var didComplete=!1;var startTime=new Date();var beginning=startTime.getTime();var totalTime=0;if(!debugMode){window[gtm4wp_datalayer_name].push({'event':'gtm4wp.reading.articleLoaded'})}else{console.log('Article loaded')}
+function trackLocation(){bottom=$(window).height()+$(window).scrollTop();height=$(document).height();if(bottom>readerLocation&&!scroller){currentTime=new Date();scrollStart=currentTime.getTime();timeToScroll=Math.round((scrollStart-beginning)/1000);if(!debugMode){window[gtm4wp_datalayer_name].push({'event':'gtm4wp.reading.startReading','timeToScroll':timeToScroll})}else{console.log('Started reading '+timeToScroll)}
+scroller=!0}
+if(bottom>=$('#'+gtm4wp_scrollerscript_contentelementid).scrollTop()+$('#'+gtm4wp_scrollerscript_contentelementid).innerHeight()&&!endContent){currentTime=new Date();contentScrollEnd=currentTime.getTime();timeToContentEnd=Math.round((contentScrollEnd-scrollStart)/1000);if(!debugMode){window[gtm4wp_datalayer_name].push({'event':'gtm4wp.reading.contentBottom','timeToScroll':timeToContentEnd})}else{console.log('End content section '+timeToContentEnd)}
+endContent=!0}
+if(bottom>=height&&!didComplete){currentTime=new Date();end=currentTime.getTime();totalTime=Math.round((end-scrollStart)/1000);if(!debugMode){if(totalTime<gtm4wp_scrollerscript_scannertime){window[gtm4wp_datalayer_name].push({'event':'gtm4wp.reading.readerType','readerType':'scanner'})}else{window[gtm4wp_datalayer_name].push({'event':'gtm4wp.reading.readerType','readerType':'reader'})}
+window[gtm4wp_datalayer_name].push({'event':'gtm4wp.reading.pagebottom','timeToScroll':totalTime})}else{if(totalTime<gtm4wp_scrollerscript_scannertime){console.log('The visitor seems to be a "scanner"')}else{console.log('The visitor seems to be a "reader"')}
+console.log('Bottom of page '+totalTime)}
+didComplete=!0}}
+$(window).scroll(function(){if(timer){clearTimeout(timer)}
+timer=setTimeout(trackLocation,callBackTime)})})
